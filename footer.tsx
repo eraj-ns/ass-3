d2003af4aca99a1a05168331ea9ea1b8d1fe9c79
Eraj <ruksanasattar054@gmail.com>
2024-11-10 16:44:27 +0500

    Add files via upload

---
+++ footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function Footer () {
+  return (
+    <div className="footerfirst">
+      <div className="footersecond">
+        <ul>
+          <li>Made By Eraj Naz</li> 
+        </ul>
+    </div>
+  </div>
+  )
+}
